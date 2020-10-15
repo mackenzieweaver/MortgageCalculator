20fedf35c25ac2e5763f35b12c2eac2ac1f7ae48
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2020-10-15 15:41:45 -0700

    New JS

---
+++ Scripts/CustomJS.js
@@ -0,0 +1,15 @@
+﻿
+function Calculate(loan, term, rate) {
+
+    //Total Monthly Payment = (amount loaned) * (rate/1200) / (1 – (1 + rate/1200)(-Number of Months) )
+    let total = (loan * (rate / 1200)) / (1 - Math.pow((1 + rate / 1200), -Math.abs(term)));
+    console.log(total);
+}
+
+//let remBalance = total - 
+
+//let intPayment = 
+
+//let principal Payment = Total Monthly Payment-Interset
+
+//End of Month remaining Balance = Previous Remaining Balance - Principal payments
