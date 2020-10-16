de85e4126f661d25a1320a5b1816f02d3c549636
Mack <macknz7@gmail.com>
2020-10-15 19:57:26 -0700

    Precison variable created / used

--- Scripts/CustomJS.js
+++ Scripts/CustomJS.js
@@ -5,6 +5,8 @@ function Calculate(loan, term, rate) {
     while (table.firstChild) {
         table.removeChild(table.firstChild);
     }
+    // So money is rounded to 2 decimal places
+    let precision = 2;
     // Total Principal = loan
     document.getElementById("totalprincipal").innerHTML = `${accounting.formatMoney(loan)}`;
     //Total Monthly Payment = (amount loaned) * (rate/1200) / (1 – (1 + rate/1200)(-Number of Months) )
@@ -27,26 +29,26 @@ function Calculate(loan, term, rate) {
         cell.appendChild(text);
         // Payment
         cell = row.insertCell();
-        text = document.createTextNode(accounting.formatMoney(totalMonthlyPayment.toFixed(2)));
+        text = document.createTextNode(accounting.formatMoney(totalMonthlyPayment.toFixed(precision)));
         cell.appendChild(text);
         // Principal
         cell = row.insertCell();
-        text = document.createTextNode(accounting.formatMoney(principalPayment.toFixed(2)));
+        text = document.createTextNode(accounting.formatMoney(principalPayment.toFixed(precision)));
         cell.appendChild(text);
         // Interest
         cell = row.insertCell();
-        text = document.createTextNode(accounting.formatMoney(interestPayment.toFixed(2)));
+        text = document.createTextNode(accounting.formatMoney(interestPayment.toFixed(precision)));
         cell.appendChild(text);
         // Total Interest
         cell = row.insertCell();
-        text = document.createTextNode(accounting.formatMoney(totalInterest.toFixed(2)));
+        text = document.createTextNode(accounting.formatMoney(totalInterest.toFixed(precision)));
         cell.appendChild(text);
         // Balance
         cell = row.insertCell();
-        text = document.createTextNode(accounting.formatMoney(remainingBalance.toFixed(2)));
+        text = document.createTextNode(accounting.formatMoney(remainingBalance.toFixed(precision)));
         cell.appendChild(text);
     }
     let totalCost = parseInt(loan) + totalInterest;
-    document.getElementById("totalinterest").innerHTML = `${accounting.formatMoney(totalInterest.toFixed(2))}`;
+    document.getElementById("totalinterest").innerHTML = `${accounting.formatMoney(totalInterest.toFixed(precision))}`;
     document.getElementById("totalcost").innerHTML = `${accounting.formatMoney(totalCost)}`;
 }
