ee34191d945d220f5f0db28db56d6f4b929c0e46
Mack <macknz7@gmail.com>
2020-10-15 19:17:03 -0700

    Total Monthly Payment, Total Principal, Total Interest, and Total Cost complete
    also added Favicon

--- Scripts/CustomJS.js
+++ Scripts/CustomJS.js
@@ -1,9 +1,25 @@
 ﻿
+// Calculate calls all other functions
 function Calculate(loan, term, rate) {
+    // Total Principal = loan
+    document.getElementById("totalprincipal").innerHTML = `${accounting.formatMoney(loan)}`;
 
     //Total Monthly Payment = (amount loaned) * (rate/1200) / (1 – (1 + rate/1200)(-Number of Months) )
-    let total = (loan * (rate / 1200)) / (1 - Math.pow((1 + rate / 1200), -Math.abs(term)));
-    console.log(total);
+    let totalMonthlyPayment = (loan * (rate / 1200)) / (1 - Math.pow((1 + rate / 1200), -Math.abs(term)));
+    document.getElementById("monthlypayment").innerHTML = `${accounting.formatMoney(totalMonthlyPayment.toFixed(2))}`;
+
+    // before the very first month equals the amount of the loan
+    let remainingBalance = loan;
+    let totalInterest = 0;
+    for (let i = 0; i < term; i++) {
+        let interestPayment = remainingBalance * (rate / 1200);
+        totalInterest += interestPayment;
+        let principalPayment = totalMonthlyPayment - interestPayment;
+        remainingBalance -= principalPayment;
+    }
+    let totalCost = parseInt(loan) + totalInterest;
+    document.getElementById("totalinterest").innerHTML = `${accounting.formatMoney(totalInterest.toFixed(2))}`;
+    document.getElementById("totalcost").innerHTML = `${accounting.formatMoney(totalCost)}`;
 }
 
 //let remBalance = total - 
