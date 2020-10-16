1c316c17a2b337953abd43d7b910394641842639
Mack <macknz7@gmail.com>
2020-10-15 19:40:11 -0700

    Table Creation: #, Montly Payment, Principal, Interest, Total Interest, Balance

--- Scripts/CustomJS.js
+++ Scripts/CustomJS.js
@@ -11,21 +11,42 @@ function Calculate(loan, term, rate) {
     // before the very first month equals the amount of the loan
     let remainingBalance = loan;
     let totalInterest = 0;
-    for (let i = 0; i < term; i++) {
+
+    const table = document.getElementById("tbody");
+    for (let i = 1; i <= term; i++) {
         let interestPayment = remainingBalance * (rate / 1200);
         totalInterest += interestPayment;
         let principalPayment = totalMonthlyPayment - interestPayment;
         remainingBalance -= principalPayment;
+
+        // Create row in table
+        let row = table.insertRow();
+        row.setAttribute("scope", "row");
+        let cell = row.insertCell();
+        let text = document.createTextNode(i.toString());
+        cell.appendChild(text);
+        // Payment
+        cell = row.insertCell();
+        text = document.createTextNode(accounting.formatMoney(totalMonthlyPayment.toFixed(2)));
+        cell.appendChild(text);
+        // Principal
+        cell = row.insertCell();
+        text = document.createTextNode(accounting.formatMoney(principalPayment.toFixed(2)));
+        cell.appendChild(text);
+        // Interest
+        cell = row.insertCell();
+        text = document.createTextNode(accounting.formatMoney(interestPayment.toFixed(2)));
+        cell.appendChild(text);
+        // Total Interest
+        cell = row.insertCell();
+        text = document.createTextNode(accounting.formatMoney(totalInterest.toFixed(2)));
+        cell.appendChild(text);
+        // Balance
+        cell = row.insertCell();
+        text = document.createTextNode(accounting.formatMoney(remainingBalance.toFixed(2)));
+        cell.appendChild(text);
     }
     let totalCost = parseInt(loan) + totalInterest;
     document.getElementById("totalinterest").innerHTML = `${accounting.formatMoney(totalInterest.toFixed(2))}`;
     document.getElementById("totalcost").innerHTML = `${accounting.formatMoney(totalCost)}`;
 }
-
-//let remBalance = total - 
-
-//let intPayment = 
-
-//let principal Payment = Total Monthly Payment-Interset
-
-//End of Month remaining Balance = Previous Remaining Balance - Principal payments
