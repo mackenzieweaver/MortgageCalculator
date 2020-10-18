b6e735d5099125cf9d80662f50e009270e1b0732
Mack <macknz7@gmail.com>
2020-10-17 23:46:22 -0700

    Mobile Navigation Toggle Implementation

---
+++ Scripts/togglenav.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+
+        $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+        $('.animated-icon3').toggleClass('open');
+    });
+});
